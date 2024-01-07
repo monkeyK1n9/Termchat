669f96b56535ba0e369d3d0131fa95f54058a444
monkeyK1n9 <mkeyk1n9@gmail.com>
2024-01-07 15:33:55 +0100

    create group member

---
+++ Termchat/Models/GroupAdmin.cs
@@ -0,0 +1,14 @@
+﻿namespace Termchat;
+
+public class GroupAdmin : User
+{
+
+    public GroupAdmin(string username) : base(username)
+    {
+
+    }
+    public override void SendMessage()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Termchat/Models/GroupMember.cs
@@ -0,0 +1,13 @@
+﻿namespace Termchat;
+
+public class GroupMember : User
+{
+    public GroupMember(string username) : base(username)
+    {
+
+    }
+    public override void SendMessage()
+    {
+        throw new NotImplementedException();
+    }
+}
