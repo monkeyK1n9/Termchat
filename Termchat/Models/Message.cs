df91115f88342fb064ed766c314ea107de30daeb
monkeyK1n9 <mkeyk1n9@gmail.com>
2024-01-07 15:19:03 +0100

    create message model

---
+++ Termchat/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿namespace Termchat;
+
+public class Message
+{
+    public int MessageId { get; set; }
+    public string Sender { get; set; }
+    public string MessageBody { get; set; }
+
+    public Message(string sender, string messageBody)
+    {
+        Sender = sender;
+        MessageBody = messageBody;
+    }
+}
--- Termchat/Models/User.cs
+++ Termchat/Models/User.cs
@@ -2,12 +2,25 @@
 
 abstract public class User
 {
+    public int UserId { get; private set; }
     public string Username { get; set; }
 
+    public User()
+    {
+        Username = GenerateRandomUsername();
+    }
     public User(string username)
     {
         Username = username;
     }
 
     abstract public void SendMessage();
+
+    private string GenerateRandomUsername()
+    {
+        Guid guid = Guid.NewGuid();
+        string username = guid.ToString()[0..8];
+
+        return username;
+    }
 }
