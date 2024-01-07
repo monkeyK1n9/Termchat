cbc776ec89a07540ea55e812b267c095d8a7e393
monkeyK1n9 <mkeyk1n9@gmail.com>
2024-01-07 14:58:42 +0100

    create abstract user

---
+++ Termchat/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Termchat;
+
+abstract public class User
+{
+    public string Username { get; set; }
+
+    public User(string username)
+    {
+        Username = username;
+    }
+
+    abstract public void SendMessage();
+}
